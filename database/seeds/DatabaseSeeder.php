a2e44176f4123a1b8dca3db32d7921ee6e19f36b
Ivica Nikolic <ivicanikolic.mail@gmail.com>
2020-04-05 21:27:00 +0200

    Popunjavanej baze korisnik i pitanja sa modelom factories i seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        factory(User::class, 3)->create()->each(function($u){
+            $u->questions()
+              ->saveMany(
+                    factory(App\Question::class, rand(1, 5))->make()
+              );  
+        });
     }
 }
