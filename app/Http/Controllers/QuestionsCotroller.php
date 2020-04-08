c6f5fed223f7e1aeaacba434590fabc6a88ac42f
Ivica Nikolic <ivicanikolic.mail@gmail.com>
2020-04-08 20:00:50 +0200

    Azuriranje pitanja i pravljenej edit stranice

--- app/Http/Controllers/QuestionsCotroller.php
+++ app/Http/Controllers/QuestionsCotroller.php
@@ -42,7 +42,7 @@ class QuestionsCotroller extends Controller
     {
         $request->user()->questions()->create($request->only('title', 'body'));
 
-        return redirect()->route('questions.index')->with('success', "Tvoje bitanje je snimljeno");
+        return redirect()->route('questions.index')->with('success', "Tvoje pitanje je snimljeno");
     }
 
     /**
@@ -64,7 +64,7 @@ class QuestionsCotroller extends Controller
      */
     public function edit(Question $question)
     {
-        //
+        return view('questions.edit', compact('question'));
     }
 
     /**
@@ -74,9 +74,11 @@ class QuestionsCotroller extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $question->update($request->only('title', 'body'));
+
+        return redirect()->route('questions.index')->with('success', "Tvoje pitanje je azurirano");
     }
 
     /**
---
+++ resources/views/questions/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h2>Edit pitanja</h2>
+                        <div class="ml-auto">
+                            <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Vrati se na sva pitanja</a>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div class="card-body">
+                    <form action="{{ route('questions.update', $question->id) }}" method="post">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group">
+                            <label for="question-title">Question Title</label>
+                            <input type="text" name="title" id="question-title" value="{{ $question->title }}"  class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
+
+                            @if ($errors->has('title'))
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('title') }}</strong>
+                                </div>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label for="question-body">Postavite vase pitanje</label>
+                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}">{{ $question->title }}</textarea>
+
+                            @if ($errors->has('body'))
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('body') }}</strong>
+                                </div>
+                            @endif
+
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-outline-primary btn-lg">Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
