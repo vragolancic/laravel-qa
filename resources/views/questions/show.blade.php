81563743c5739ef87effd5823a595b2b4e9b7368
Ivica Nikolic <ivicanikolic.mail@gmail.com>
2020-04-10 19:46:53 +0200

    Dodavanje odgovora i prikazivanje podataka o korisniku

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -16,8 +16,50 @@
                 </div>
 
                 <div class="card-body">
-                    {{$question->body_html}}
+                    {!! $question->body_html !!}
+                    <div class="float-right">
+                        <span class="text-muted">Answerd {{ $question->created_date }}</span>
+                        <div class="media">
+                            <a href="{{ $question->user->url }}" class="pr-2">
+                                <img width="40" src="https://ramcotubular.com/wp-content/uploads/default-avatar.jpg" >
+                            </a>
+                            <div class="media-body mt-1">
+                                <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card mt-2">
+                 <div class="card-body">
+                     <div class="card-title">
+                        <h2>{{ $question->answers_count ." ".   Str::plural('Answer', $question->answers_count)}}</h2>
+                     </div>
+                     <hr>
+                     @foreach ($question->answers as $answer)
+                         <div class="media">
+                                <div class="media-body">
+                                    {!! $answer->body_html !!}
+                                    <div class="float-right">
+                                        <span class="text-muted">Answerd {{ $answer->created_date }}</span>
+                                        <div class="media">
+                                            <a href="{{ $answer->user->url }}" class="pr-2">
+                                                <img width="40" src="https://ramcotubular.com/wp-content/uploads/default-avatar.jpg" >
+                                            </a>
+                                            <div class="media-body mt-1">
+                                                <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                        </div>
+                         <hr>
+                     @endforeach
+                 </div>
             </div>
         </div>
     </div>
