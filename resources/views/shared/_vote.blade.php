5c3f5ecf2a4d2542b9b062ca5b23d0bd8cecc597
Ivica Nikolic <ivicanikolic.mail@gmail.com>
2020-04-16 20:11:21 +0200

    popravljane urija u _vote

--- resources/views/shared/_vote.blade.php
+++ resources/views/shared/_vote.blade.php
@@ -10,15 +10,20 @@
     @endphp
 @endif
 
+@php
+   $formId = $name . "-" .$model->id;
+   $formAction = "/{$firstURISegment}/{$model->id}/vote";
+@endphp
+
 <div class="d-flex flex-column vote-controls">
     <a title="Ovo pitanje je korisno" 
     class="vote-up {{ Auth::guest() ? 'off' : ''}}"
-    onclick="event.preventDefault(); document.getElementById('up-vote-{{$name}}-{{ $model->id }}').submit();"
+    onclick="event.preventDefault(); document.getElementById('up-vote-{{ $formId }}').submit();"
     >
         <i class="fa fa-caret-up fa-3x"></i>
     </a>
 
-    <form action="/{{$firstURISegment}}/{{$model->id}}/vote" style="display: none;" id="up-vote-{{$name}}-{{ $model->id }}" method="post">
+    <form action="{{ $formAction }}" style="display: none;" id="up-vote-{{ $formId }}" method="post">
         @csrf
         <input type="hidden" name="vote" value="1">
     </form>
@@ -27,11 +32,11 @@
 
     <a title="Ovo pitanje je nekorisno" 
         class="vote-down {{ Auth::guest() ? 'off' : ''}}"
-        onclick="event.preventDefault(); document.getElementById('down-vote-{{$name}}-{{ $model->id }}').submit();"
+        onclick="event.preventDefault(); document.getElementById('down-vote-{{ $formId }}').submit();"
         >
         <i class="fa fa-caret-down fa-3x"></i>
     </a>
-    <form action="/{{$firstURISegment}}/{{$model->id}}/vote" style="display: none;" id="down-vote-{{$name}}-{{ $model->id }}" method="post">
+    <form action="{{ $formAction }}" style="display: none;" id="down-vote-{{ $formId }}" method="post">
         @csrf
         <input type="hidden" name="vote" value="-1">
     </form>
